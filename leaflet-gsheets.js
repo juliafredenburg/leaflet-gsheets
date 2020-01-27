4ad546612ceedde709d37a11f617baa56b741175
Julia Fredenburg <julia.fredenburg@gmail.com>
2020-01-26 19:37:11 -0500

    updated links to google sheets

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -11,48 +11,49 @@ function init() {
   // these URLs come from Google Sheets 'shareable link' form
   // the first is the polygon layer and the second the points
   var polyURL =
-    "https://docs.google.com/spreadsheets/d/1EUFSaqi30b6oefK0YWWNDDOzwmCTTXlXkFHAc2QrUxM/edit?usp=sharing";
+    'https://docs.google.com/spreadsheets/d/1g9767cJeIJljN6EQU19djG1k1wFmhv4S0eAeuoDC2Ho/edit?usp=sharing';
   var pointsURL =
-    "https://docs.google.com/spreadsheets/d/1kjJVPF0LyaiaDYF8z_x23UulGciGtBALQ1a1pK0coRM/edit?usp=sharing";
+    'https://docs.google.com/spreadsheets/d/1IDwky8nXzzclVtQk_nXXt4NO_tFZ23skoZZDxbNW9rU/edit?usp=sharing';
 
   Tabletop.init({ key: polyURL, callback: addPolygons, simpleSheet: true });
   Tabletop.init({ key: pointsURL, callback: addPoints, simpleSheet: true }); // simpleSheet assumes there is only one table and automatically sends its data
 }
-window.addEventListener("DOMContentLoaded", init);
+
+window.addEventListener('DOMContentLoaded', init);
 
 // Create a new Leaflet map centered on the continental US
-var map = L.map("map").setView([40, -100], 4);
+var map = L.map('map').setView([40, -100], 4);
 
 // This is the Carto Positron basemap
 var basemap = L.tileLayer(
-  "https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}{r}.png",
+  'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}{r}.png',
   {
     attribution:
       "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://cartodb.com/attributions'>CartoDB</a>",
-    subdomains: "abcd",
-    maxZoom: 19
+    subdomains: 'abcd',
+    maxZoom: 19,
   }
 );
 basemap.addTo(map);
 
 var sidebar = L.control
   .sidebar({
-    container: "sidebar",
+    container: 'sidebar',
     closeButton: true,
-    position: "right"
+    position: 'right',
   })
   .addTo(map);
 
-let panelID = "my-info-panel";
+let panelID = 'my-info-panel';
 var panelContent = {
   id: panelID,
   tab: "<i class='fa fa-bars active'></i>",
   pane: "<p id='sidebar-content'></p>",
-  title: "<h2 id='sidebar-title'>No state selected</h2>"
+  title: "<h2 id='sidebar-title'>No state selected</h2>",
 };
 sidebar.addPanel(panelContent);
 
-map.on("click", function() {
+map.on('click', function () {
   sidebar.close(panelID);
 });
 
@@ -73,60 +74,63 @@ function addPolygons(data) {
   // Start with an empty GeoJSON of type FeatureCollection
   // All the rows will be inserted into a single GeoJSON
   var geojsonStates = {
-    type: "FeatureCollection",
-    features: []
+    type: 'FeatureCollection',
+    features: [],
   };
 
   for (var row in data) {
     // The Sheets data has a column 'include' that specifies if that row should be mapped
-    if (data[row].include == "y") {
+    if (data[row].include == 'y') {
       var coords = JSON.parse(data[row].geometry);
 
       geojsonStates.features.push({
-        type: "Feature",
+        type: 'Feature',
         geometry: {
-          type: "MultiPolygon",
-          coordinates: coords
+          type: 'MultiPolygon',
+          coordinates: coords,
         },
         properties: {
           name: data[row].name,
           summary: data[row].summary,
           state: data[row].state,
-          local: data[row].local
-        }
+          local: data[row].local,
+        },
       });
     }
   }
 
   // The polygons are styled slightly differently on mouse hovers
-  var polygonStyle = { color: "#2ca25f", fillColor: "#99d8c9", weight: 1.5 };
-  var polygonHoverStyle = { color: "green", fillColor: "#2ca25f", weight: 3 };
+  var polygonStyle = { color: '#2ca25f', fillColor: '#99d8c9', weight: 1.5 };
+  var polygonHoverStyle = { color: 'green', fillColor: '#2ca25f', weight: 3 };
 
   polygonLayer = L.geoJSON(geojsonStates, {
-    onEachFeature: function(feature, layer) {
+    onEachFeature: function (feature, layer) {
       layer.on({
-        mouseout: function(e) {
+        mouseout: function (e) {
           e.target.setStyle(polygonStyle);
         },
-        mouseover: function(e) {
+
+        mouseover: function (e) {
           e.target.setStyle(polygonHoverStyle);
         },
-        click: function(e) {
+
+        click: function (e) {
           // This zooms the map to the clicked polygon
           // map.fitBounds(e.target.getBounds());
 
           // if this isn't added, then map.click is also fired!
           L.DomEvent.stopPropagation(e);
 
-          document.getElementById("sidebar-title").innerHTML =
+          document.getElementById('sidebar-title').innerHTML =
             e.target.feature.properties.name;
-          document.getElementById("sidebar-content").innerHTML =
+          document.getElementById('sidebar-content').innerHTML =
             e.target.feature.properties.summary;
           sidebar.open(panelID);
-        }
+        },
       });
     },
-    style: polygonStyle
+
+    style: polygonStyle,
   }).addTo(map);
 }
 
@@ -136,6 +140,7 @@ function addPoints(data) {
   if (pointGroupLayer != null) {
     pointGroupLayer.remove();
   }
+
   pointGroupLayer = L.layerGroup().addTo(map);
 
   for (var row = 0; row < data.length; row++) {
@@ -150,27 +155,27 @@ function addPoints(data) {
     marker.feature = {
       properties: {
         location: data[row].location,
-        category: data[row].category
-      }
+        category: data[row].category,
+      },
     };
     marker.on({
-      click: function(e) {
+      click: function (e) {
         L.DomEvent.stopPropagation(e);
-        document.getElementById("sidebar-title").innerHTML =
+        document.getElementById('sidebar-title').innerHTML =
           e.target.feature.properties.location;
-        document.getElementById("sidebar-content").innerHTML =
+        document.getElementById('sidebar-content').innerHTML =
           e.target.feature.properties.category;
         sidebar.open(panelID);
-      }
+      },
     });
 
     // AwesomeMarkers is used to create fancier icons
     var icon = L.AwesomeMarkers.icon({
-      icon: "info-sign",
-      iconColor: "white",
+      icon: 'info-sign',
+      iconColor: 'white',
       markerColor: getColor(data[row].category),
-      prefix: "glyphicon",
-      extraClasses: "fa-rotate-0"
+      prefix: 'glyphicon',
+      extraClasses: 'fa-rotate-0',
     });
     marker.setIcon(icon);
   }
@@ -180,11 +185,11 @@ function addPoints(data) {
 // Used for the points layer
 function getColor(type) {
   switch (type) {
-    case "Coffee Shop":
-      return "green";
-    case "Restaurant":
-      return "blue";
+    case 'Coffee Shop':
+      return 'green';
+    case 'Restaurant':
+      return 'blue';
     default:
-      return "green";
+      return 'green';
   }
 }
